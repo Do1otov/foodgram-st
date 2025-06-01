7a0899bb60e58f0d0afbb638398a1b2ae1e1f664
Do1otov <dolotok2@gmail.com>
2025-06-01 15:47:02 +0300

    Delete group from admin, small fix readme

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
 
 from .models import Subscription, User
+
+
+admin.site.unregister(Group)
 
 
 @admin.register(User)
